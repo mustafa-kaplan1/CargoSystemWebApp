ed22ef421c89b99e11acda9a00b25d6767f09752
Mustafa Kaplan <mkaplan2005@gmail.com>
2025-12-18 14:39:45 +0300

    inital commit. Structure setup

---
+++ CargoSystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CargoSystem.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
